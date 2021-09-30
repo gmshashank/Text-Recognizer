f0f749b6530340da56dc435738f23809215270e7
Shashank Mewada <shashank.mewada@ampedv.com>
2021-09-30 01:11:58 +0100

    MNIST training

--- text_recognizer/models/mlp.py
+++ text_recognizer/models/mlp.py
@@ -40,6 +40,6 @@ class MLP(nn.Module):
 
     @staticmethod
     def add_to_argparse(parser):
-        parser.add_to_argument("--fc1", type=int, default=1024)
-        parser.add_to_argument("--fc2", type=int, default=128)
+        parser.add_argument("--fc1", type=int, default=1024)
+        parser.add_argument("--fc2", type=int, default=128)
         return parser

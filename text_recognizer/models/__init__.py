6062ff2d2a28e7edaa9e4e9dd1bf3df85cfc601c
Shashank Mewada <shashank.mewada@ampedv.com>
2021-10-06 00:36:57 +0100

    IMplemented Bidirectional Transformer.

--- text_recognizer/models/__init__.py
+++ text_recognizer/models/__init__.py
@@ -5,4 +5,15 @@ from .line_cnn_lstm import LineCNNLSTM
 
 # Hide lines below until Lab 3
 from .line_cnn_simple import LineCNNSimple
+
+# Hide lines below until Lab 4
+from .line_cnn_transformer import LineCNNTransformer
 from .mlp import MLP
+
+# Hide lines above until Lab 2
+
+
+# Hide lines above until Lab 3
+
+
+# Hide lines above until Lab 4
